35bbd7f0c7525fe56bd4ce6367bd9143424c1396
HaoxiangWu <117926387+HaoxiangWu@users.noreply.github.com>
2026-07-31 19:33:29 +0200

    init: push code first time (#1)
    feat: add cvhub object detection

---
+++ main.py
@@ -0,0 +1,10 @@
+import torch
+
+def main():
+    print(torch.__version__)
+    print(torch.cuda.is_available())
+    print(torch.cuda.get_device_name(0))
+
+
+if __name__ == "__main__":
+    main()
